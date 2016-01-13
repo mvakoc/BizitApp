b5346758f18c82e1d7531c46d5002bc4181bb436
Michael Vakoc <vakoc.m@gmail.com>
2016-01-13 09:52:58 +0100

    Initial commit

---
+++ BizitApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BizitApp.MvcApplication" Language="C#" %>
---
+++ BizitApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BizitApp.Startup))]
+namespace BizitApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
